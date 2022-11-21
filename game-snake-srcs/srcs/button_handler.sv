789c45191c41368d1992794d8787549c5f6ac58d
Ulisses Maffazioli <ulissesmaffa@gmail.com>
2022-11-20 22:19:56 -0300

    Inicio Button_handler

--- game-snake-srcs/srcs/button_handler.sv
+++ game-snake-srcs/srcs/button_handler.sv
@@ -4,9 +4,28 @@ dos botões existentes na placa.
 */
 
 module button_handler
+import game_snake_pkg::*;
 (
     input logic clk
   , input logic rst
+  , input logic up_btn
+  , input logic down_btn
+  , input logic right_btn
+  , input logic left_btn
+  , input logic direction_type
 );
+
+always @(posedge clk)
+begin
+    if (~rst) begin
+      direction_type <= D_RIGHT; 
+    end else begin
+      /*intervalo de tempo*/
+        /*escolhe nova posição 
+        ou segue na mesma posição*/
+
+    end
+
+end
 
 endmodule : button_handler
---
+++ game-snake-srcs/srcs/game_snake_pkg.sv
@@ -0,0 +1,9 @@
+package game_snake_pkg;
+  typedef enum  logic [3:0] {
+    D_UP,
+    D_DOWN,
+    D_RIGHT,
+    D_LEFT
+  } direction_type;  // Decoded instruction in decode
+
+endpackage : game_snake_pkg
